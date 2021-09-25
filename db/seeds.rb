831e4b0a8a74132d0e8a44372abb35c6aacecc09
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2021-09-25 18:26:27 +0530

    db: clean up old references

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,1 @@
 # frozen_string_literal: true
-
-Role.create(title: 'Admin', id: 1, description: 'Default admin role', scope: roles_config[:default_admin]) unless Role.exists?(id: 1)
-Role.create(title: 'User', id: 2, description: 'Default normal role', scope: roles_config[:default_user]) unless Role.exists?(id: 2)
-
